a3eb4c369ca7e672746e87d0bd9f154954cbf2c8
codewithfaizan <16091973083@lords.ac.in>
2022-01-01 23:59:19 +0530

    hello

--- JAN/DAY1/helloworld.c
+++ JAN/DAY1/helloworld.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-<<<<<<< HEAD
-int main() { printf("Hello World !"); }
-=======
 int main() {
   printf("Hello World !\n");
->>>>>>> ee93221cad8efad08bc1ce40a9ae6cc1d9479e4b
 
   return 0;
 }
